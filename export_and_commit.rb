3e463bccd7cefbf3d18edd85f3770c3accaebc99
Jevon Wright <jevon@jevon.org>
2018-11-21 18:02:01 +1300

    Add a quick bbcode -> markdown code formatter
    Still a WIP

--- export_and_commit.rb
+++ export_and_commit.rb
@@ -102,6 +102,69 @@ def process_edit_or_page(row)
   git_add_and_commit(filename, git_updated, git_author, git_reason)
 end
 
+def generate_markdown(bbcode)
+  # Ignore the frontmatter
+  frontmatter = bbcode.split("---", 3)
+  if frontmatter.length != 3
+    fail "Expected three parts in #{bbcode}"
+  end
+  markdown = frontmatter.last.strip
+
+  # Headers
+  markdown = markdown
+    .gsub(/^==== *(.+) *====$/i, "#### \\1")
+    .gsub(/^=== *(.+) *===$/i, "### \\1")
+    .gsub(/^== *(.+) *==$/i, "## \\1")
+    .gsub(/^= *(.+) *=$/i, "# \\1")
+
+  # Italics, bold
+  markdown = markdown
+    .gsub(/'''(.+?)'''/i, "**\\1**")
+    .gsub(/''(.+?)''/i, "_\\1_")
+
+  # Code blocks
+  markdown = markdown
+    .gsub(/\[code\]([^\n]+?)\[\/code\]/i, "`\\1`") # single-line version
+    .gsub(/\[code ([^\]]+)\]\n*(.+?)\n*\[\/code\]/im, "```\\1\n\\2\n```")
+    .gsub(/\[code\]\n*(.+?)\n*\[\/code\]/im, "```\n\\1\n```")
+
+  # Replace [[link|title]] with [markdown links](markdown link)
+  markdown = markdown.gsub(/\[\[([^|\[\]]+)\|([^|\[\]]+)\]\]/i) do |match|
+    "[#{$2}](#{select_slug($1)}.md)"
+  end
+
+  # Replace [[link]] with [markdown links](markdown link)
+  markdown = markdown.gsub(/\[\[([^|\[\]]+)\]\]/i) do |match|
+    "[#{$1}](#{select_slug($1)}.md)"
+  end
+
+  # Replace old {{include|var=value}} format with jekyll {% include filename %}
+  # markdown = markdown.gsub(/\{\{([^|}]+)\|(.+?)=(.*)\}\}/i) do |match|
+  #   # "{% include #{select_slug($1)}.md #{$2}=\"#{$3}\" %}"
+  #   "<!-- #{match} -->"
+  # end
+
+  # # Replace old {{include|random text}} format with jekyll {% include filename %}
+  # markdown = markdown.gsub(/\{\{([^|}]+)\|(.+?)\}\}/i) do |match|
+  #   # "{% include #{select_slug($1)}.md comment=\"#{$2}\" %}"
+  #   "<!-- #{match} -->"
+  # end
+
+  # Replace old {{include}} format with jekyll {% include filename %}
+  markdown = markdown.gsub(/\{\{([^}]+)\}\}/i) do |match|
+    # "{% include #{select_slug($1)}.md %}"
+    "<!-- #{match.gsub("{{", "include ").gsub("}}", "")} -->"
+  end
+
+  # Join them all back together
+  return "---
+#{frontmatter[1].strip}
+---
+
+#{markdown}
+"
+end
+
 db = Mysql2::Client.new(username: db_username, password: db_password, database: db_name)
 
 # First: create edits, which are old page versions
@@ -116,5 +179,26 @@ edits.each do |row|
   process_edit_or_page(row)
 end
 
-# TODO create redirections as appropriate
+# Finally, do a quick and dirty bbcode -> markdown reformat
+Dir.glob("wiki/**/old-content/**/*.md") do |filename|
+  input = File.read(filename)
+
+  markdown = generate_markdown(input)
+
+  puts "Rewriting #{filename} from bbcode (#{input.length}) to markdown (#{markdown.length})"
+
+  File.open(filename, 'w') do |fp|
+    fp.write(markdown)
+  end
+
+  # We don't create an individual commit for each markdown; that's useless.
+  # It's much better to have a single commit that does ALL of it at once.
+  # We'll leave it up to the person running the script to do the commit.
+
+  # unless $no_pause_between_commits
+  #   puts "Press enter to continue..."
+  #   ignored = STDIN.gets
+  # end
+end
+
 # TODO reformat bbcode format as markdown format automatically
